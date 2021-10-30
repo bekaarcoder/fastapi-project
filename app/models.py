512192b23b3bc1e3ba55575d247d04dd8f73385a
Shashank <s_shashank@outlook.com>
2021-10-30 22:43:59 +0530

    added vote model

--- app/models.py
+++ app/models.py
@@ -32,3 +32,14 @@ class User(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+
+
+class Vote(Base):
+    __tablename__ = "vote"
+
+    user_id = Column(
+        Integer, ForeignKey("user.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("post.id", ondelete="CASCADE"), primary_key=True
+    )
