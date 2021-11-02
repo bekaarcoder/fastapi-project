01c3665586e72ce5449363c7cadb2612d8d3e91e
Shashank <s_shashank@outlook.com>
2021-11-02 21:30:36 +0530

    updated database url

--- app/database.py
+++ app/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-SQLALCHEMY_DATABASE_URL = settings.database_uri
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# SQLALCHEMY_DATABASE_URL = settings.database_uri
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
